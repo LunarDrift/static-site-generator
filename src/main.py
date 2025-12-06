b3b37833db2361ae11e141333ea99cbb3f0b7e57
Travis <mynameismyname1992@gmail.com>
2025-12-06 10:16:44 -0600

    Ready to deploy

--- src/main.py
+++ src/main.py
@@ -2,18 +2,26 @@ import os
 import shutil
 
 from copystatic import copy_contents
+from gencontent import generate_pages_recursively
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main():
+    # Delete anything in the public directory
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
-
+    # Copy all static files from static to public
     print("Copying static directory to public directory...")
     copy_contents(dir_path_static, dir_path_public)
+    print("Done!")
+    # Generate a page from content/index.md using template.html and write it to public/index.html
+    print("Generating content...")
+    generate_pages_recursively(dir_path_content, template_path, dir_path_public)
     print("Done!")
 
 main()
